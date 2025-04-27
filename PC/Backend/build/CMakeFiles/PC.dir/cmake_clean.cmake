883a91a5459261c336a193db0bda735e807ebcb2
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-04-27 10:07:00 +0200

    Project setup in VSCode

---
+++ PC/Backend/build/CMakeFiles/PC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PC.dir/link.d"
+  "CMakeFiles/PC.dir/main.c.o"
+  "CMakeFiles/PC.dir/main.c.o.d"
+  "PC"
+  "PC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
